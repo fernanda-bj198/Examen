0db6a586d7b315ea24d1fba1342f405dd538ce34
fernanda-bj198 <badillo.juarez.fer.ofimatica@gmail.com>
2020-10-02 14:03:23 -0500

    primer comit

---
+++ contenido.php
@@ -0,0 +1,46 @@
+
+<?php 
+
+
+	function contenido(){
+
+		$datos=array();
+
+
+		$datos[0]="img/img1.jpg"."||".
+		          "Laptop HP Core i7 8 GB RAM 512GB SSD + 32 GB Optane"."||".
+		          "Procesador Intel Core 10ª Gen: Inicia tu laptop y apps en pocos segundos
+                   Windows 10 Home:Haz cosas increíbles con la sensación familiar de Windows, solo que mejorada";
+
+	    $datos[1]="img/img2.jpg"."||".
+		          "Laptop HP Pentium Gold 8 GB RAM 1 TB"."||".
+		          "Procesador Intel Pentium Gold de doble núcleo: 
+		           Gran rendimiento, consumo y rentabilidad
+                   Pantalla Alta Definición: 
+                   Disfruta de tu mundo digital de un modo totalmente nuevo";   
+		          
+	    $datos[2]="img/img3.jpg"."||".
+		          "Desktop DELL Core i3 6 GB RAM 1 TB"."||".
+		          "530GB de disco duro 
+		           Micrófono digital integrado
+                   Audio HD con altavoces estéreo";     
+
+		$datos[3]="img/img4.jpg"."||".
+		          "Laptop Asus Intel Celeron RAM 4 GB 128 EMMC"."||".
+		          "Almacenamiento eMMC: 
+		           Con su almacenamiento eMMC tendrás un menor consumo de energía
+                   SonicMaster:
+                   Tecnología Audio SonicMaster para una experiencia potente y clara
+                   Micrófono digital integrado";   
+
+		$datos[4]="img/img5.jpg"."||".
+		          "Laptop Asus TUF Ryzen 7 RAM 8 GB 1 TB HDD"."||".
+		          "Optimizada para Gaming y escritura
+                   Con procesador Ryzen 7 que mejora el rendimiento de tu Asus
+                   Nvidia GeForce GTX 1650 que mejora el desempeño gráfico y optimiza la ejecución de tus juegos";
+
+        return $datos;
+
+	}
+
+ ?>
